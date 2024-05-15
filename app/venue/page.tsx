62954e923863454842ee487927ba9083dc48bd4d
Douglas Odell <douglasio711@gmail.com>
2024-05-15 15:39:39 -0700

    Revert "npm > yarn"
    This reverts commit c2ccb89e64e1c9e3efd04701c42aa0b12caff5a1.

--- app/venue/page.tsx
+++ app/venue/page.tsx
@@ -5,6 +5,12 @@ const Venue = () => {
 		<div>
 			<h1>Venue</h1>
 			<p>Hello World! This is the Venue page</p>
+			<img
+				src="/images/center_leaves.jpg"
+				alt=""
+				height="auto"
+				width="500"
+			/>
 			<p>
 				Visit the <Link href="/about">About</Link> page.
 			</p>
