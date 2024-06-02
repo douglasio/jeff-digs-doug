5dff5ddb58e7abe578e631bc442e1af8c17f5fdd
Douglas Odell <douglasio711@gmail.com>
2024-06-01 22:27:59 -0700

    Accept multiple passwords

--- functions/cfp_login.ts
+++ functions/cfp_login.ts
@@ -3,18 +3,28 @@ import { sha256, getCookieKeyValue } from "./utils";
 
 export async function onRequestPost(context: {
 	request: Request;
-	env: { CFP_PASSWORD?: string };
+	env: {
+		CFP_PASSWORD1?: string;
+		CFP_PASSWORD2?: string;
+		CFP_PASSWORD3?: string;
+	};
 }): Promise<Response> {
 	const { request, env } = context;
 	const body = await request.formData();
 	const { password, redirect } = Object.fromEntries(body);
 	const hashedPassword = await sha256(password.toString());
-	const hashedCfpPassword = await sha256(env.CFP_PASSWORD as string);
+	const hashedCfpPassword1 = await sha256(env.CFP_PASSWORD1 as string);
+	const hashedCfpPassword2 = await sha256(env.CFP_PASSWORD2 as string);
+	const hashedCfpPassword3 = await sha256(env.CFP_PASSWORD3 as string);
 	const redirectPath = redirect.toString() || "/";
 
-	if (hashedPassword === hashedCfpPassword) {
+	if (
+		hashedPassword === hashedCfpPassword1 ||
+		hashedPassword === hashedCfpPassword2 ||
+		hashedPassword === hashedCfpPassword3
+	) {
 		// Valid password. Redirect to home page and set cookie with auth hash.
-		const cookieKeyValue = await getCookieKeyValue(env.CFP_PASSWORD);
+		const cookieKeyValue = await getCookieKeyValue(env.CFP_PASSWORD1);
 
 		return new Response("", {
 			status: 302,
