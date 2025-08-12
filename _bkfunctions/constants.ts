0185484a63303ebe6d91197f566785dc72fbdbef
Douglas Odell <douglasio711@gmail.com>
2025-08-11 17:44:04 -0700

    Incorporate open-next for CF Workers compat

---
+++ _bkfunctions/constants.ts
@@ -0,0 +1,22 @@
+/**
+ * Key for the auth cookie.
+ */
+export const CFP_COOKIE_KEY = "CFP-Auth-Key";
+
+/**
+ * Max age of the auth cookie in seconds.
+ * Default: 1 week.
+ */
+export const CFP_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
+
+/**
+ * Paths that don't require authentication.
+ * The /cfp_login path must be included.
+ */
+export const CFP_ALLOWED_PATHS = [
+	"/",
+	"/venue",
+	"/things-to-know",
+	"/cfp_login",
+	"/our-story",
+];
