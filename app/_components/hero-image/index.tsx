f468e0260ba9190927b4dd0c016b3fcc10b09c32
Douglas Odell <douglasio711@gmail.com>
2025-08-18 10:55:46 -0700

    Add loading blur to all photos, dress code images

--- app/_components/hero-image/index.tsx
+++ app/_components/hero-image/index.tsx
@@ -6,16 +6,13 @@ import { Box, rem } from "@mantine/core";
 import { Image, SVG } from "_components";
 import { mobileNavBreakpoint } from "_util";
 
-const blurDataURL =
-	"data:image/webp;base64,UklGRsYCAABXRUJQVlA4WAoAAAAgAAAAgAAAVQAASUNDUMgBAAAAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAH4AABAAEAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxADZWUDgg2AAAAJAIAJ0BKoEAVgA+7W6uUKm3KacrU2qC4B2JaW7dW70C/JhJ/J6NmptjXHU7nB1XcimhEgzApl+D/+jGS+iZE2y0qlKV7LmMKMgtf5xAAP7rCGpDHh+u9mrU97S3e9BZXezMCf+t67okZ8wjv6yvmUczPveE2iBYinHM6UNUJhtVffbfhLYKAEVFnUrt7nubsF/6ED+wShOzUVzbw65ayGg/i5o4Q7z+RNR9e28lagoOAZpFgmXaPOvCnRtr3ay9nbnYEv4Rdz7EUk7r97chu3LcLHVANAAAAA==";
-
 export const HeroImage = () => {
 	const [isHeroLoaded, setIsHeroLoaded] = useState(false);
 
 	return (
 		<Box pos="relative" w="100%" h="100%">
 			<motion.div
-				initial={{ opacity: 0 }}
+				initial={{ opacity: 0.25 }}
 				animate={isHeroLoaded ? { y: 0, opacity: 1 } : {}}
 				transition={{ duration: 1, ease: "easeOut" }}
 				style={{ height: "100%" }}
@@ -28,8 +25,6 @@ export const HeroImage = () => {
 					height={800}
 					h={{ base: "100%", [mobileNavBreakpoint]: "100vh" }}
 					src="/static/images/webp/engagement_1.webp"
-					placeholder="blur"
-					blurDataURL={blurDataURL}
 					onLoad={() => setIsHeroLoaded(true)}
 					priority
 				/>
