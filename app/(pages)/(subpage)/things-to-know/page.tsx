f02419abf9fe390b246cf527348a2977232e405e
Douglas Odell <douglasio711@gmail.com>
2025-08-23 08:27:24 -0700

    Typo fix, observability flag enabled

--- app/(pages)/(subpage)/things-to-know/page.tsx
+++ app/(pages)/(subpage)/things-to-know/page.tsx
@@ -102,8 +102,7 @@ const things = [
 
 				<Text mt="sm">
 					Use the link above to get the room block rates listed below,
-					or book wherever wherever you want, you&rsquo;re an
-					independent lady.
+					or book wherever you want, you&rsquo;re an independent lady.
 				</Text>
 
 				<Title order={5}>Rates:</Title>
